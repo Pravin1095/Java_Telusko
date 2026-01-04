bd050d03d06835031108591751ee1bcbd38ba1fa
Pravin A <a.pravin3210@gmail.com>
2026-01-04 18:50:18 +0530

    Object Creation and Scopes(SingleTon and Prototype)

--- ExploringSpringFramework/src/main/java/com/teluskoLearning/Alien.java
+++ ExploringSpringFramework/src/main/java/com/teluskoLearning/Alien.java
@@ -2,6 +2,7 @@ package com.teluskoLearning;
 
 public class Alien {
 
+    int age;
     public void code(){
         System.out.println("Coding..");
     }
--- ExploringSpringFramework/src/main/java/com/teluskoLearning/App.java
+++ ExploringSpringFramework/src/main/java/com/teluskoLearning/App.java
@@ -15,8 +15,17 @@ public class App
         //We need to say to spring to create object of class Alien.
         //There are several ways for this , we can use Java configuration,XML or annotations.
         //Here we use annotations to inform spring to create objects for our classes
+        //Whenever we call ClassPathXMLAllpicationContext, passing our xml file, a bean
+        //will be created for it
+        //Note: if we have defined alien class twice in xml bean will be created twice.
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-        Alien obj = (Alien) context.getBean("alien");
-        obj.code();
+        Alien obj1 = (Alien) context.getBean("alien"); // The name here should match the id that we give in the xml file
+        obj1.age = 21;
+        obj1.code();
+        System.out.println(obj1.age);
+
+        Alien obj2 = (Alien) context.getBean("alien");
+        obj2.code();
+        System.out.println(obj2.age);
     }
 }
