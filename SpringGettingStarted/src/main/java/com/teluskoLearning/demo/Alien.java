59aa80bd829a3377c4c4e13eb84b2741f237b2ad
Pravin A <a.pravin3210@gmail.com>
2026-01-04 14:23:23 +0530

    Autowiring in SpringBoot

--- SpringGettingStarted/src/main/java/com/teluskoLearning/demo/Alien.java
+++ SpringGettingStarted/src/main/java/com/teluskoLearning/demo/Alien.java
@@ -1,10 +1,18 @@
 package com.teluskoLearning.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Alien {
+
+    //@Autowired is used to get the object of class Laptop from Spring IOC container
+    //In main we used Application context to get the Bean but here with just Autowired
+    //we can wire between Alien and Laptop class and get the Bean of Laptop class
+    @Autowired
+    Laptop laptop;
+
     public void code(){
-        System.out.println("Start coding");
+        laptop.compile();
     }
 }
