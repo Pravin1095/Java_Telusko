8b2999e82fddc7526c57a8769c1215d9b88c53cb
Pravin A <a.pravin3210@gmail.com>
2026-01-04 14:15:45 +0530

    Dependency Injection using SpringBoot

--- SpringGettingStarted/src/main/java/com/teluskoLearning/demo/DemoApplication.java
+++ SpringGettingStarted/src/main/java/com/teluskoLearning/demo/DemoApplication.java
@@ -2,13 +2,24 @@ package com.teluskoLearning.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(DemoApplication.class, args);
+		//Application Context is an interface that helps to create dependency injection
+		//Dependency injection is a concept where spring itself creates, maintains and destroys
+		//objects for us so that we can focus only on business logics.
+		//The created beans will be stored in IOC(Inversion of control) container.
+		//Spring will not create all the class objects. For it to create an object of a class
+		//we need to mention @Component annotation on top of the class.
+        //Hence if Bean doesn't exist it gives NoSuchBeanDefinitionException.
+
+		ApplicationContext context = SpringApplication.run(DemoApplication.class, args);
+		Alien obj = context.getBean(Alien.class);
+		obj.code();
 	}
 
 }
