0b464c95584d19a4860caf5345395f25ae2991c4
Pravin A <a.pravin3210@gmail.com>
2026-01-04 21:47:30 +0530

    LazyInit Bean

--- ExploringSpringFramework/src/main/java/com/teluskoLearning/Desktop.java
+++ ExploringSpringFramework/src/main/java/com/teluskoLearning/Desktop.java
@@ -1,8 +1,9 @@
 package com.teluskoLearning;
 
-import org.springframework.stereotype.Component;
-
 public class Desktop implements Computer {
+    public Desktop(){
+        System.out.println("Desktop object created");
+    }
     @Override
     public void compile(){
         System.out.println("Compiling using Desktop");
