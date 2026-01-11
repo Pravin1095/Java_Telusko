1e5fa5cc833d149baed70d70079258390e7d6033
Pravin A <a.pravin3210@gmail.com>
2026-01-11 18:17:22 +0530

    Responding to the Client

--- servletExample/src/main/java/com/telusko_springbootweb/HelloServlet.java
+++ servletExample/src/main/java/com/telusko_springbootweb/HelloServlet.java
@@ -4,12 +4,17 @@ import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 // The service method will be called whenever a request is sent from the browser to our service
 //Tomcat default port is 8080
 public class HelloServlet extends HttpServlet {
 
-    public void service(HttpServletRequest req, HttpServletResponse res){
+    public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+        out.println("<h2><b>Hello World</b></h2>");
         System.out.println("Service method called");
     }
 }
