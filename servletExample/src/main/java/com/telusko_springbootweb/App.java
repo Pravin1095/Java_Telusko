0ab4a50104f78c67eab16e14d483af322c31f3c0
Pravin A <a.pravin3210@gmail.com>
2026-01-11 12:02:22 +0530

    Servlet mapping to client

--- servletExample/src/main/java/com/telusko_springbootweb/App.java
+++ servletExample/src/main/java/com/telusko_springbootweb/App.java
@@ -1,5 +1,6 @@
 package com.telusko_springbootweb;
 
+import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
 
@@ -14,10 +15,17 @@ public class App
         //We need to strart the tomcat server and also keep it running with
         //await so that browser sends a request and servlet's service method will
         //be called
-        
+
         Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8081);
+
+        Context context = tomcat.addContext("",null);
+        Tomcat.addServlet(context, "HelloServlet", new HelloServlet());
+        context.addServletMappingDecoded("/hello", "HelloServlet");
         tomcat.start();
         tomcat.getServer().await();
+
+
         System.out.println( "Hello World!" );
     }
 }
