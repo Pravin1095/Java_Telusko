450dfa1f322f1a2212aaa65f9fca3016eb092cd7
Pravin A <a.pravin3210@gmail.com>
2026-04-16 22:00:46 +0530

    Sending CSRF token

--- SpringSecurity/src/main/java/com/teluskolearning/springsecurity_demo/StudentController.java
+++ SpringSecurity/src/main/java/com/teluskolearning/springsecurity_demo/StudentController.java
@@ -1,5 +1,7 @@
 package com.teluskolearning.springsecurity_demo;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +17,11 @@ public class StudentController {
             new Student(1, "Pravin", "React"),
             new Student(2, "Navin", "Java")
     ));
+
+    @GetMapping("csrf-token")
+    public CsrfToken getCsrfToken(HttpServletRequest request){
+        return (CsrfToken) request.getAttribute("_csrf");
+    }
 
     @GetMapping("students")
     public List<Student> getStudents(){
